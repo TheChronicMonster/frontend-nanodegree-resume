f914f4405ce8f887a4bdd41b4d2e27d9c90b7010
TheChronicMonster <legendzing@gmail.com>
2015-05-29 19:17:50 -0500

    Update resumeBuilder.js
    Begin with four objects. Bio, Work, Education, Projects

---
+++ js/resumeBuilder.js
@@ -0,0 +1,107 @@
+var bio = {
+	"name" : "JP Miller",
+	"skills" : ["writing", "marketing", "management", "front-end design & development"],
+	"role" : "Executive Content Director at www.urbangamingelite.com",
+	"bioPic" : "images/bioImage.jpg",
+	"welcomeMessage" : "I love writing, coding and learning. My goal is to create excellent user experiences.",
+	"contactInfo" : {
+		"twitter" : "@theChronicMonst",
+		"email" : "legendzing@gmail.com",
+		"mobile" : "903-293-6594",
+		"location" : "Madison, WI",
+		"github" : "github.com/theChronicMonster",
+		"linkedIn" : "linkedin.com/in/jpmillerii",
+		"relocation" : "Willing to relocate"
+	}
+}
+
+var work = {
+	"Experience": [
+		{
+			"Employer": "Urban Gaming Elite",
+			"Title": "Executive Content Director",
+			"Dates": "August 2014 - Present",
+			"Description": "I strategize with UGEs founder, Alex, manage our freelance writers and push live content to the website."
+		},
+		{
+			"Employer": "Self Employed",
+			"Title": "Freelance Writer",
+			"Dates": "February 2013 - Present",
+			"Description": "I manage and motivate myself to produce quality content for the internet for my clients."
+		},
+		{
+			"Employer": "Self Employed",
+			"Title": "Independent Courier",
+			"Dates": "January 2012 - December 2014",
+			"Description": "I delivered consumer and agricultural goods as well as bank deposits on regularly scheduled routes in the Greater Chicago Region."
+		}
+	]
+}
+
+var education = {
+	"Schools": [
+		{
+			"Name" : "Udacity",
+			"Degree" : "Nanodegree",
+			"Major" : "Front-End Web Development",
+			"Societies" : "Inaugural Cohort",
+			"Url" : "http://www.udacity.com"
+		},
+		{
+			"Name" : "The Second City",
+			"Degree" : "One Year Certificate",
+			"Major" : "Improvisational Acting",
+			"Societies" : "The Scribes",
+			"Url" : "http://www.secondcity.com/courses/chicago/adult/"
+		},
+		{
+			"Name" : "Texas A&M University-Texarkana",
+			"Degree" : "Bachelor",
+			"Major" : "Applied Arts & Sciences",
+			"Societies" : "Student Leadership Development Program",
+			"Url" : "http://tamut.edu/index.php"
+		},
+		{
+			"Name" : "Texarkana College",
+			"Degree" : "Associate",
+			"Major" : "Business Marketing",
+			"Societies" : "Phi Theta Kappa",
+			"Url" : "https://www.texarkanacollege.edu/"
+		}
+	]
+}
+
+var projects = {
+	"projects": [
+		{
+			"title" : "Portfolio Mockup",
+			"dates" : "November 2014",
+			"description" : "I created a web friendly mockup from a pdf file. Project utilizes HTML, Bootstrap and a JavaScript Modal.",
+			"image" : "https://github.com/TheChronicMonster/Mug-MockUp/blob/master/images/mug.png"
+		},
+		{
+			"title" : "Frogger Game Clone",
+			"dates" : "February 2015",
+			"description" : "A point for nostalgia. I recreated the classic frogger game using boys and bugs.",
+			"image" : "https://github.com/TheChronicMonster/Frogger-Clone/blob/master/images/char-boy.png"
+		},
+		{
+			"title" : "Website Optimization",
+			"dates" : "March 2015",
+			"description" : "I began with a very clunky webpage and optimizated it to render within milliseconds.",
+			"image" : "https://github.com/TheChronicMonster/website-optimization/blob/master/views/images/pizza.png"
+		},
+		{
+			"title" : "Feed Reader Testing with JasmineJS",
+			"dates" : "April 2015",
+			"description" : "JasmineJS provides an excellent way to bug proof code with test driven development. I used the red-green testing method to ensure that every aspect of the web page worked as expected.",
+			"image" : "http://derickbailey.com/wp-content/uploads/2014/04/jasmine.png"
+		},
+		{
+			"title" : "Neighborhood Map",
+			"dates" : "May 2015",
+			"description" : "The Neighborhood Map incorporates the Google Maps and FourSquare APIs to create a map of interesting locations in my community.",
+			"image" : "http://cdn.thedroidguy.com/wp-content/uploads/2013/09/new-google-maps-logo.png"
+		}
+	]
+}
